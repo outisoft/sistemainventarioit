d9e4eb9e3842a692d6a0027ec83345c1b4a36f15
Antonio Bartolon <abartolon@grupo-pinero.com>
2025-01-15 12:03:27 -0500

    Implementacion de tickets

--- resources/views/equipos/complements/create.blade.php
+++ resources/views/equipos/complements/create.blade.php
@@ -2,7 +2,7 @@
 <div class="modal fade" id="modalCreate" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            
+
             <form method="POST" action="{{ route('complements.store') }}">
                 @csrf
                 <div class="modal-header">
@@ -12,79 +12,76 @@
                 </div>
 
                 <div class="modal-body">
-                        <!-- Tipo -->
-                        <div class="mb-3">
-                            <x-input-label class="form-label" for="type_id" :value="__('Type equipment')" />
-                            <select id="type_id" name="type_id" class="form-control" aria-label="Default select example">
-                                <option value="1">SCANNER</option>
-                                <option value="5">MONITOR</option>
-                                <option value="6">MOUSE</option>
-                                <option value="7">NO-BREACK</option>
-                                <option value="8">TECLADO</option>
-                                <option value="9">WACOM</option>
-                            </select>
-                        </div>
+                    <!-- Tipo -->
+                    <div class="mb-3">
+                        <x-input-label class="form-label" for="type_id" :value="__('Type equipment')" />
+                        <select id="type_id" name="type_id" class="form-control" aria-label="Default select example">
+                            <option value="5">MONITOR</option>
+                            <option value="6">MOUSE</option>
+                            <option value="7">NO-BREACK</option>
+                            <option value="1">SCANNER</option>
+                            <option value="8">TECLADO</option>
+                            <option value="14">TICKETERA</option>
+                            <option value="9">WACOM</option>
+                        </select>
+                    </div>
 
-                        <!-- Marca -->
-                        <div class="mb-3">
-                            <x-input-label class="form-label" for="brand" :value="__('Brand')" />
-                            <div class="input-group input-group-merge">
-                                <x-text-input id="brand" class="form-control" type="text"
-                                    name="brand" placeholder="HP" :value="old('brand')" required
-                                    autocomplete="brand" />
-                            </div>
-                            <x-input-error :messages="$errors->get('brand')" class="mt-2" />
+                    <!-- Marca -->
+                    <div class="mb-3">
+                        <x-input-label class="form-label" for="brand" :value="__('Brand')" />
+                        <div class="input-group input-group-merge">
+                            <x-text-input id="brand" class="form-control" type="text" name="brand"
+                                placeholder="HP" :value="old('brand')" required autocomplete="brand" />
                         </div>
+                        <x-input-error :messages="$errors->get('brand')" class="mt-2" />
+                    </div>
 
-                        <!-- Model -->
-                        <div class="mb-3">
-                            <x-input-label class="form-label" for="model" :value="__('Model')" />
-                            <div class="input-group input-group-merge">
-                                <x-text-input id="model" class="form-control" type="text"
-                                    name="model" placeholder="Lazer MFP 432fdm" :value="old('model')" required
-                                    autocomplete="model" />
-                            </div>
-                            <x-input-error :messages="$errors->get('model')" class="mt-2" />
+                    <!-- Model -->
+                    <div class="mb-3">
+                        <x-input-label class="form-label" for="model" :value="__('Model')" />
+                        <div class="input-group input-group-merge">
+                            <x-text-input id="model" class="form-control" type="text" name="model"
+                                placeholder="Lazer MFP 432fdm" :value="old('model')" required autocomplete="model" />
                         </div>
+                        <x-input-error :messages="$errors->get('model')" class="mt-2" />
+                    </div>
 
-                        <!-- Numero de serie -->
-                        <div class="mb-3">
-                            <x-input-label class="form-label" for="serial" :value="__('Serial number')" />
-                            <div class="input-group input-group-merge">
-                                <x-text-input id="serial" class="form-control" type="text"
-                                    name="serial" placeholder="CNB1P50T0" :value="old('serial')" required
-                                    autocomplete="serial" />
-                            </div>
-                            <x-input-error :messages="$errors->get('serial')" class="mt-2" />
+                    <!-- Numero de serie -->
+                    <div class="mb-3">
+                        <x-input-label class="form-label" for="serial" :value="__('Serial number')" />
+                        <div class="input-group input-group-merge">
+                            <x-text-input id="serial" class="form-control" type="text" name="serial"
+                                placeholder="CNB1P50T0" :value="old('serial')" required autocomplete="serial" />
                         </div>
+                        <x-input-error :messages="$errors->get('serial')" class="mt-2" />
+                    </div>
 
-                        {{-- Región (solo visible para administradores) --}}
-                        @role('Administrator')
+                    {{-- Región (solo visible para administradores) --}}
+                    @role('Administrator')
 
                         <!-- Region -->
                         <div class="mb-3">
                             <x-input-label class="form-label" for="region_id" :value="__('REGION')" />
                             <select class="form-control" id="region_id" name="region_id">
                                 <option value="">Choose a region</option>
-                                @foreach($regions as $region)
-                                    <option value="{{ $region->id }}" 
-                                            {{ old('region_id') == $region->id ? 'selected' : '' }}>
+                                @foreach ($regions as $region)
+                                    <option value="{{ $region->id }}"
+                                        {{ old('region_id') == $region->id ? 'selected' : '' }}>
                                         {{ $region->name }}
                                     </option>
                                 @endforeach
                             </select>
                             <x-input-error :messages="$errors->get('region_id')" class="mt-2" />
                         </div>
-                            
-                        @else
-                            <input type="hidden" name="region_id" value="{{ auth()->user()->region_id }}">
-                        @endrole
+                    @else
+                        <input type="hidden" name="region_id" value="{{ auth()->user()->region_id }}">
+                    @endrole
                 </div>
 
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-primary">Save</button>
                 </div>
-            
+
             </form>
         </div>
     </div>
