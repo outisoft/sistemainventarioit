8c064044b42fb2b5969ccc9ceb800937a537a654
jacuba98 <jacuba98456@mail.com>
2025-01-14 21:04:15 -0500

    Backup details3

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('backup:run')->everyMinute();
+        $schedule->command('backup:run --only-db')->daily();
     }
 
     /**
