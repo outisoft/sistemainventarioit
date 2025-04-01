5c1ee730b2ac46dd3caa93736e431a2fc9386594
Antonio Bartolon <abartolon@grupo-pinero.com>
2025-04-01 13:17:53 -0500

    Eliminacion de telescope3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,9 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local')) {
-            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-        }
+        //
     }
 
     /**
