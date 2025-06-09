ff5dbc980f76fb5d5597c4298e1f75d03f2ae1b4
Antonio Bartolon <abartolon@grupo-pinero.com>
2025-06-09 09:20:54 -0500

    Detalles del home

--- app/Exports/EmpleadosExport.php
+++ app/Exports/EmpleadosExport.php
@@ -2,7 +2,7 @@
 
 namespace App\Exports;
 
-use App\Models\Empleado;
+use App\Models\position;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
@@ -10,30 +10,30 @@ class EmpleadosExport implements FromCollection, WithHeadings
 {
     public function collection()
     {
-        $empleados = Empleado::with('hotel', 'departments', 'equipos.complements', 'equipos.license')->get();
+        $positions = Position::with('hotel', 'departments', 'equipments.complements', 'equipments.license')->get();
 
-        return $empleados->flatMap(function ($empleado) {
-            return $empleado->equipos->map(function ($equipo) use ($empleado) {
+        return $positions->flatMap(function ($position) {
+            return $position->equipments->map(function ($equipo) use ($position) {
                 return [
-                    'No. employee' => $empleado->no_empleado,
-                    'Name' => $empleado->name,
-                    'Email' => $empleado->email,
-                    'Hotel' => $empleado->hotel->name ?? 'N/A',
-                    'Department' => $empleado->departments->name ?? 'N/A',
-                    'Job' => $empleado->puesto,
-                    'AD' => $empleado->ad,
+                    'No. employee' => $position->employee->no_employee ?? 'N/A',
+                    'Name' => $position->employee->name ?? 'N/A',
+                    'Email' => $position->email,
+                    'Hotel' => $position->hotel->name ?? 'N/A',
+                    'Department' => $position->departments->name ?? 'N/A',
+                    'Job' => $position->position,
+                    'AD' => $position->ad,
                     'Equipment - Type' => $equipo->tipo->name ?? 'N/A',
                     'Equipment - Brand' => $equipo->marca ?? 'N/A',
                     'Equipment - Model' => $equipo->model ?? 'N/A',
                     'Equipment - Serial' => $equipo->serial ?? 'N/A',
                     'Equipment - Name' => $equipo->name ?? 'N/A',
                     'Equipment - IP' => $equipo->ip ?? 'N/A',
-                    'Complements - Type' => $empleado->equipos->flatMap->complements->pluck('type.name')->filter()->join(', '),
-                    'Complements - Brand' => $empleado->equipos->flatMap->complements->pluck('brand')->filter()->join(', '),
-                    'Complements - Model' => $empleado->equipos->flatMap->complements->pluck('model')->filter()->join(', '),
-                    'Complements - Serial' => $empleado->equipos->flatMap->complements->pluck('serial')->filter()->join(', '),
-                    'License - Type' => $empleado->equipos->flatMap->license->pluck('type')->filter()->join(', '),
-                    'License - Key' => $empleado->equipos->flatMap->license->pluck('key')->filter()->join(', '),
+                    'Complements - Type' => $position->equipments->flatMap->complements->pluck('type.name')->filter()->join(', '),
+                    'Complements - Brand' => $position->equipments->flatMap->complements->pluck('brand')->filter()->join(', '),
+                    'Complements - Model' => $position->equipments->flatMap->complements->pluck('model')->filter()->join(', '),
+                    'Complements - Serial' => $position->equipments->flatMap->complements->pluck('serial')->filter()->join(', '),
+                    'License - Type' => $position->equipments->flatMap->license->pluck('type')->filter()->join(', '),
+                    'License - Key' => $position->equipments->flatMap->license->pluck('key')->filter()->join(', '),
                 ];
             });
         });
