363d5d05a0d2273020496480f5d2f234782ceea2
Antonio Bartolon <abartolon@grupo-pinero.com>
2025-06-11 12:06:24 -0500

    Detalles en licencias3

--- resources/views/licenses/show.blade.php
+++ resources/views/licenses/show.blade.php
@@ -37,13 +37,12 @@
                                         class='icon-base bx bx-desktop'></i>{{ $equipo->name }}
                                 </h6>
                                 <address class="mb-0">
-                                    @if ($equipo->empleados->isNotEmpty())
-                                        {{ $equipo->empleados->first()->name }}
-                                    @else
-                                        SIN ASIGNAR
-                                    @endif <br>
-                                    @if ($equipo->empleados->isNotEmpty() && $equipo->empleados->first()->hotel)
-                                        {{ $equipo->empleados->first()->hotel->name }}-{{ $equipo->empleados->first()->departments->name }}<br>
+                                    @foreach ($equipo->positions as $position)
+                                        {{ $position->employee->name ?? 'N/A' }} -
+                                        ({{ $position->position }})
+                                    @endforeach <br>
+                                    @if ($equipo->positions->isNotEmpty() && $equipo->positions->first()->hotel)
+                                        {{ $equipo->positions->first()->hotel->name }}-{{ $equipo->positions->first()->departments->name }}<br>
                                     @else
                                         HOTEL NO ASIGNADO<br>
                                     @endif
@@ -96,11 +95,10 @@
                                                         {{ $equipo->serial }}
                                                     </td>
                                                     <td>
-                                                        @if ($equipo->empleados->isNotEmpty())
-                                                            {{ $equipo->empleados->first()->name }}
-                                                        @else
-                                                            Sin asignar
-                                                        @endif
+                                                        @foreach ($equipo->positions as $position)
+                                                            {{ $position->employee->name ?? 'N/A' }} -
+                                                            ({{ $position->position }})
+                                                        @endforeach
                                                     </td>
                                                     <td>
                                                         @if ($equipo->license->where('type_id', $licencia->type_id)->isEmpty())
