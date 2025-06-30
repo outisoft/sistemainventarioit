b5cbacfb32c44c1bcb0e0f728d7d06d925774b52
Antonio Bartolon <abartolon@grupo-pinero.com>
2025-06-30 10:30:06 -0500

    Detalles de qr

--- resources/views/assignment/qrcode.blade.php
+++ resources/views/assignment/qrcode.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,7 +18,8 @@
             min-height: 100vh;
             margin: 0;
         }
-            /* From Uiverse.io by Cobp */ 
+
+        /* From Uiverse.io by Cobp */
         .card {
             position: relative;
             display: flex;
@@ -28,6 +30,7 @@
             gap: 0.5rem;
             height: max-content;
         }
+
         .card_form {
             position: relative;
             width: 18.75em;
@@ -37,29 +40,31 @@
             transition: 0.2s ease-in-out;
             overflow: hidden;
         }
+
         .card_form span {
             font-size: 1.5em;
             position: absolute;
             inset: 0;
             padding: 5px 10px;
             color: #fff;
-            background-image: linear-gradient(
-                to top,
-                rgba(0, 0, 0, 0) 0%,
-                rgba(0, 0, 0, 0.7) 100%
-            );
+            background-image: linear-gradient(to top,
+                    rgba(0, 0, 0, 0) 0%,
+                    rgba(0, 0, 0, 0.7) 100%);
             opacity: 0;
             transition: all 0.2s ease-in-out;
         }
+
         .card:hover .card_form span,
         .card:hover .card_data span {
             opacity: 1;
         }
+
         .card_data {
             display: flex;
             align-items: center;
             justify-content: space-between;
         }
+
         .card_data span {
             color: #b5a160;
             display: flex;
@@ -69,31 +74,38 @@
             opacity: 0;
             cursor: pointer;
         }
+
         .card_data span:hover {
             text-decoration: underline;
         }
+
         .text {
             display: flex;
             justify-content: center;
             flex-direction: column;
             color: white;
         }
+
         .text_m {
             font-size: 0.9em;
         }
+
         .text_s {
             color: #b5a160;
             font-size: 0.6em;
         }
+
         .cube {
             width: max-content;
             height: 10px;
             transition: all 0.2s;
             transform-style: preserve-3d;
         }
+
         .card:hover .cube {
             transform: rotateX(90deg);
         }
+
         .side {
             width: max-content;
             height: 1em;
@@ -104,13 +116,14 @@
             letter-spacing: 0.5px;
             font-weight: bold;
         }
+
         .top {
             transform: rotateX(-90deg) translate3d(0, 0, 0em);
         }
+
         .front {
             transform: translate3d(0, 0, 1em);
         }
-
     </style>
     <script>
         function printQR() {
@@ -118,28 +131,31 @@
         }
     </script>
 </head>
+
 <body>
-<div class="card">
-    <div class="card_form">
-        <span>{{ $position->ad }}</span>
-        <img src="data:image/png;base64,{{ base64_encode($qrcode) }}" alt="QR Code">
-    </div>
-    <div class="card_data">
-        <div style="display: flex" class="data">
-            <div class="text">
-                <label class="text_m">{{ $position->employee->name}}</label>
-                <div class="cube text_s">
-                <label class="side front"> {{ $position->email }} </label>
-                <label class="side top">{{ $position->position }} </label>
+    <div class="card">
+        <div class="card_form">
+            <span>{{ $position->ad }}</span>
+            <img src="data:image/png;base64,{{ base64_encode($qrcode) }}" alt="QR Code">
+        </div>
+        <div class="card_data">
+            <div style="display: flex" class="data">
+                <div class="text">
+                    <label class="text_m">{{ $position->employee->name ?? 'N/A' }}</label>
+
+                    <div class="cube text_s">
+                        <label class="side front"> {{ $position->email }} </label>
+                        <label class="side top">{{ $position->position }} </label>
+                    </div>
                 </div>
             </div>
+            <span title="Descargar codigo QR">
+                <a href="{{ route('downloadQRCode', $position->id) }}"
+                    download="qrcode_employee_{{ $position->id }}.png">Descargar QR</a>
+            </span>
         </div>
-        <span title="Descargar codigo QR">
-            <a href="{{ route('downloadQRCode', $position->id) }}" download="qrcode_employee_{{ $position->id }}.png">Descargar QR</a>
-        </span>
     </div>
-</div>
 
 </body>
-</html>
 
+</html>
