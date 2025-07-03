1350428412aa61efb832fc80afe493c6b6ff6a28
Antonio Bartolon <abartolon@grupo-pinero.com>
2025-07-03 12:19:07 -0500

    details HTTPS-2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Livewire::component('global-search', \App\Livewire\GlobalSearch::class);
 
-        //if($this->app->environment('production')) {
+        if($this->app->environment('production')) {
             \URL::forceScheme('https');
-        //}
+        }
     }
 }
